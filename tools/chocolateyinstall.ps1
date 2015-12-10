aaa175f6214e67ef98be94f02918e47ae4c4c825
Philip Arola <philiparola@hotmail.com>
2015-12-10 03:46:10 -0800

    Updated to 2015.12.01

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'CHIRP.install'
+$version	= '2015.12.01'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-20151201-installer.exe'
+$fileType = 'exe'
+$silentArgs = '/S'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
