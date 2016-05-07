1a0b62e4174fcdf54d89ff614ffedbc5d7fcdcbd
Philip Arola <philiparola@hotmail.com>
2016-05-07 12:18:17 -0700

    Updated to 2016.05.04

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'CHIRP.install'
-$version	= '2016.04.19'
-$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160419/chirp-daily-20160419-installer.exe'
+$version	= '2016.05.04'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160504/chirp-daily-20160504-installer.exe'
 $fileType = 'exe'
 $silentArgs = '/S'
 
