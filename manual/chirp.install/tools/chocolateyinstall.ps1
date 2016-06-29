f116a5fe93c7bf3e962a5c244a5b9f4055c3b505
Philip Arola <philiparola@hotmail.com>
2016-06-28 20:20:17 -0700

    (chirp.install) Updated to 2016.06.23
    Also changed icon link to static cdn.rawgit.com

--- manual/chirp.install/tools/chocolateyinstall.ps1
+++ manual/chirp.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'CHIRP.install'
-$version	= '2016.06.15'
-$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160615/chirp-daily-20160615-installer.exe'
+$version	= '2016.06.23'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160623/chirp-daily-20160623-installer.exe'
 $fileType = 'exe'
 $silentArgs = '/S'
 
