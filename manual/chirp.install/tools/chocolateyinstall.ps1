d5a2927ed8fa1f8b2e471df26d1a0f36bdd2433a
Philip Arola <philiparola@hotmail.com>
2016-07-24 14:21:10 -0700

    (chirp.install) Updated to 2016.07.17

--- manual/chirp.install/tools/chocolateyinstall.ps1
+++ manual/chirp.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'CHIRP.install'
-$version	= '2016.07.13'
-$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160713/chirp-daily-20160713-installer.exe'
+$version	= '2016.07.17'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160717/chirp-daily-20160717-installer.exe'
 $fileType = 'exe'
 $silentArgs = '/S'
 
