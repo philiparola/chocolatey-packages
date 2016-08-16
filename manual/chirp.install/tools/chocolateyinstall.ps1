8abe4e45d40bac9f4a455af216c08246cc5c321c
Philip Arola <philiparola@hotmail.com>
2016-08-16 09:01:54 -0700

    (chirp.install) Fix for 2016.08.02
    New version 2016.08.02.1
    Download URL was of the previous version

--- manual/chirp.install/tools/chocolateyinstall.ps1
+++ manual/chirp.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'CHIRP.install'
-$version	= '2016.08.02'
-$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160731/chirp-daily-20160731-installer.exe'
+$version	= '2016.08.02.1'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160802/chirp-daily-20160802-installer.exe'
 $fileType = 'exe'
 $silentArgs = '/S'
 
