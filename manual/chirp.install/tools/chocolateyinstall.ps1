439a484158d134aa0c9c33c5c91d79d925a6f2b6
Philip Arola <philiparola@hotmail.com>
2016-08-18 11:46:27 -0700

    (chirp.install) Updated to 2016.08.18
    Fixed misspelled summary
    Added releaseNotes and admin tag at the behest of the choco email bot

--- manual/chirp.install/tools/chocolateyinstall.ps1
+++ manual/chirp.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'CHIRP.install'
-$version	= '2016.08.16'
-$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160816/chirp-daily-20160816-installer.exe'
+$version	= '2016.08.18'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160818/chirp-daily-20160818-installer.exe'
 $fileType = 'exe'
 $silentArgs = '/S'
 
