91f922dc9798d8718842abb81fd1850871823ab4
Philip Arola <philiparola@hotmail.com>
2016-09-04 01:11:29 -0700

    (chirp.install) Updated to 2016.09.03
    Also added the packageSourceUrl tag because the emails kept yelling at me

--- manual/chirp.install/tools/chocolateyinstall.ps1
+++ manual/chirp.install/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'CHIRP.install'
-$version	= '2016.08.30'
-$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160830/chirp-daily-20160830-installer.exe'
+$version	= '2016.09.03'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160903/chirp-daily-20160903-installer.exe'
 $fileType = 'exe'
 $silentArgs = '/S'
-$checksum = 'A57F34DB2E43C1875ACF9E04CE6294FB4B10948CE276E44A0BCA479AEE94A815'
+$checksum = 'D04A7DA6D5AE26F2CA70DE17E73E659A3D0D061BFD9EAF1277A957203435C635'
 $checksumType = 'sha256'
 
 Install-ChocolateyPackage $packagename $fileType $silentArgs $url -checksum $checksum -checksumType $checksumType
