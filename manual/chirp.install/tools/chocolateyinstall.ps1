89c3448fc580f351b24cdee621fb11bf75a0525d
Philip Arola <philiparola@hotmail.com>
2016-09-04 01:10:11 -0700

    (chirp.install) Updated to 2016.08.30
    Also incorporated sha256 checksum verification

--- manual/chirp.install/tools/chocolateyinstall.ps1
+++ manual/chirp.install/tools/chocolateyinstall.ps1
@@ -1,9 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'CHIRP.install'
-$version	= '2016.08.19'
-$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160819/chirp-daily-20160819-installer.exe'
+$version	= '2016.08.30'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160830/chirp-daily-20160830-installer.exe'
 $fileType = 'exe'
 $silentArgs = '/S'
+$checksum = 'A57F34DB2E43C1875ACF9E04CE6294FB4B10948CE276E44A0BCA479AEE94A815'
+$checksumType = 'sha256'
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+Install-ChocolateyPackage $packagename $fileType $silentArgs $url -checksum $checksum -checksumType $checksumType
