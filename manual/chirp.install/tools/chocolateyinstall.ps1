dbb31ea66df284332ff14f461f40065d5d5ae510
Philip Arola <philiparola@hotmail.com>
2016-09-10 11:27:33 -0700

    (chirp.install) Updated to 2016.09.09
    SHA1 is provided on the CHIRP server

--- manual/chirp.install/tools/chocolateyinstall.ps1
+++ manual/chirp.install/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName= 'CHIRP.install'
 $version	= '2016.09.06'
-$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160906/chirp-daily-20160906-installer.exe'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20160909/chirp-daily-20160909-installer.exe'
 $fileType = 'exe'
 $silentArgs = '/S'
-$checksum = '012831CFA45E7CF816FB611D0389B71A88488B83AAF03B188F957419B10F9301'
-$checksumType = 'sha256'
+$checksum = 'd3ce6b6e18e3f4513ee3ce45c86104973c76a128'
+$checksumType = 'sha1'
 
 Install-ChocolateyPackage $packagename $fileType $silentArgs $url -checksum $checksum -checksumType $checksumType
