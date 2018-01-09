4312a2195b067201587fdd67d4b672cde6fae13a
Philip Arola <philiparola@hotmail.com>
2018-01-09 13:27:58 -0800

    (chirp.install) Updated to 2017.12.28

--- manual/chirp.install/tools/chocolateyinstall.ps1
+++ manual/chirp.install/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'CHIRP.install'
-$version	= '2017.12.04'
-$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20171204/chirp-daily-20171204-installer.exe'
+$version	= '2017.12.28'
+$url        = 'http://trac.chirp.danplanet.com/chirp_daily/daily-20171228/chirp-daily-20171228-installer.exe'
 $fileType = 'exe'
 $silentArgs = '/S'
-$checksum = '5d368a5f228f716fb5472f663fb014c1705d94c5'
+$checksum = '33c69ea8be0efb846ba55d2a405fc273bda53bc8'
 $checksumType = 'sha1'
 
 Install-ChocolateyPackage $packagename $fileType $silentArgs $url -checksum $checksum -checksumType $checksumType
