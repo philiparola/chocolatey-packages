27038b83f6710cc5c4b437f7c044f3c5d2b5e23c
Philip Arola <philiparola@hotmail.com>
2019-12-26 17:28:48 -0800

    (OpenCV) Working auto script
    Will pull and generate and push OpenCV updates to chocolatey
    Still lots of missing error handling, particularly with versioning

--- auto/opencv/opencv_update.py
+++ auto/opencv/opencv_update.py
@@ -5,6 +5,10 @@ import wget, hashlib
 import wget
 import os
 import os.path
+import fileinput
+import shutil
+import in_place
+import sys
 
 # Regex for opencv versioning
 # \d+\.{1}\d+\.{1}\d+
@@ -22,7 +26,7 @@ def main():
 	last_two_releases(releases)
 
 
-def create_package_from_release(release, asset):
+def create_package_from_release(branch, release, asset):
 	if os.path.isfile(asset.name) and DEBUG is True:
 		download = asset.name
 	else:
@@ -32,7 +36,7 @@ def create_package_from_release(release, asset):
 	with open(download,"rb") as f:
 		for byte_block in iter(lambda: f.read(BUF_SIZE),b""):
 			sha256_hash.update(byte_block)
-	choco_pack_wrapper(asset.name, asset.browser_download_url, release.tag_name, sha256_hash.hexdigest(), release.body, os.path.getsize(download))
+	choco_pack_wrapper(branch, asset.name, asset.browser_download_url, release.tag_name, sha256_hash.hexdigest(), release.body, os.path.getsize(download) >> 20)
 
 
 def last_two_releases(releases):
@@ -40,12 +44,12 @@ def last_two_releases(releases):
 	for asset in releases[0].get_assets():
 		asset_url_index = asset.name.find(".exe")
 		if (asset_url_index > 0):
-			create_package_from_release(releases[0], asset)
+			create_package_from_release("master", releases[0], asset)
 
 	for asset in releases[1].get_assets():
 		asset_url_index = asset.name.find(".exe")
 		if (asset_url_index > 0):
-			create_package_from_release(releases[1], asset)
+			create_package_from_release("3.4", releases[1], asset)
 
 
 def last_release_in_file(releases):
@@ -58,9 +62,27 @@ def specific_verion_as_param(version, releases):
 	pass
 
 
-def choco_pack_wrapper(filename, url, version_number, hash, releasenotes, filesize):
+def choco_pack_wrapper(branch, filename, url, version_number, hash, releasenotes, filesize):
 	# Create copies of the template files
+	shutil.copyfile("./" + branch + "/opencv.nuspec.template", "./" + branch + "/opencv.nuspec")
+	shutil.copyfile("./" + branch + "/tools/chocolateyinstall.ps1.template", "./" + branch + "/tools/chocolateyinstall.ps1")
 	# Change the contents of the new files with the new data
+	with in_place.InPlace("./" + branch + "/opencv.nuspec", encoding="utf-8") as file:
+		for line in file:
+			file.write(line.replace("[VERSION_NUMBER]", version_number))
+	with in_place.InPlace("./" + branch + "/opencv.nuspec", encoding="utf-8") as file:
+		for line in file:
+			file.write(line.replace("[FILESIZE]", str(filesize)))
+	with in_place.InPlace("./" + branch + "/opencv.nuspec", encoding="utf-8") as file:
+		for line in file:
+			file.write(line.replace("[RELEASE_NOTES]", releasenotes))
+
+	with in_place.InPlace("./" + branch + "/tools/chocolateyinstall.ps1", encoding="utf-8") as file:
+		for line in file:
+			file.write(line.replace("[URL]", url))
+	with in_place.InPlace("./" + branch + "/tools/chocolateyinstall.ps1", encoding="utf-8") as file:
+		for line in file:
+			file.write(line.replace("[CHECKSUM]", hash))
 	print(filename)
 	print(url)
 	print(version_number)
@@ -68,9 +90,15 @@ def choco_pack_wrapper(filename, url, version_number, hash, releasenotes, filesi
 	print(releasenotes)
 	print(filesize)
 	# Run 'choco pack' with the new files
+	os.system("cd " + branch + " && choco pack")
 	# Run 'choco push' but NOT if in debug :^)
+	if DEBUG is False:
+		os.system("cd " + branch + " && choco push OpenCV." + version_number + ".nupkg")
 	# OPTIONAL: Create Git commit?
 
 
 if __name__== "__main__":
-	main()
+	if len(sys.argv) > 0:
+		if(sys.argv[1] == "false"):
+			DEBUG = False
+		main()
